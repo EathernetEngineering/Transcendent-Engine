45e1007ea2aefb838039ab80889a3dae22115a14
sammyboy0110 <samualeather@hotmail.com>
2020-05-28 14:23:35 +1000

    Added more boxes and added scale control to sandbox
    Signed-off-by: sammyboy0110 <samualeather@hotmail.com>

--- Sandbox/src/SandboxLayer.cpp
+++ Sandbox/src/SandboxLayer.cpp
@@ -19,6 +19,14 @@ SandboxLayer::SandboxLayer() {
 	m_Position2 = { 800.0f, 200.0f };
 	m_Scale2 = { 100.0f,100.0f };
 	m_Colour2 = { 1.0f, 1.0f, 1.0f, 1.0f };
+
+	m_Position3 = { 200.0f, 200.0f };
+	m_Scale3 = { 100.0f,100.0f };
+	m_Colour3 = { 1.0f, 1.0f, 1.0f, 1.0f };
+	
+	m_Position4 = { 800.0f, 200.0f };
+	m_Scale4 = { 100.0f,100.0f };
+	m_Colour4 = { 1.0f, 1.0f, 1.0f, 1.0f };
 }
 
 void SandboxLayer::OnAttach() {
@@ -35,6 +43,8 @@ void SandboxLayer::OnUpdate() {
 	TE::Renderer2D::BeginScene(*m_Camera);
 	TE::Renderer2D::DrawQuad(m_Position1, m_Scale1, m_Colour1);
 	TE::Renderer2D::DrawQuad(m_Position2, m_Scale2, m_Colour2);
+	TE::Renderer2D::DrawQuad(m_Position3, m_Scale3, m_Colour3);
+	TE::Renderer2D::DrawQuad(m_Position4, m_Scale4, m_Colour4);
 	TE::Renderer2D::EndScene();
 }
 
@@ -43,6 +53,8 @@ void SandboxLayer::OnImGuiRender() {
 	ImGui::Begin("Box Colour");
 	ImGui::ColorEdit4("Colour 1", &m_Colour1[0]);
 	ImGui::ColorEdit4("Colour 2", &m_Colour2[0]);
+	ImGui::ColorEdit4("Colour 3", &m_Colour3[0]);
+	ImGui::ColorEdit4("Colour 4", &m_Colour4[0]);
 	ImGui::End();
 
 	ImGui::Begin("Box Tranforms");
@@ -50,15 +62,34 @@ void SandboxLayer::OnImGuiRender() {
 	ImGui::SliderFloat("Box 1 Tranform Y", &m_pos1[1], 0.0f, 620.0f);
 	ImGui::SliderFloat("Box 2 Tranform X", &m_pos2[0], 0.0f, 1180.0f);
 	ImGui::SliderFloat("Box 2 Tranform Y", &m_pos2[1], 0.0f, 620.0f);
+	ImGui::SliderFloat("Box 3 Tranform X", &m_pos3[0], 0.0f, 1180.0f);
+	ImGui::SliderFloat("Box 3 Tranform Y", &m_pos3[1], 0.0f, 620.0f);
+	ImGui::SliderFloat("Box 4 Tranform X", &m_pos4[0], 0.0f, 1180.0f);
+	ImGui::SliderFloat("Box 4 Tranform Y", &m_pos4[1], 0.0f, 620.0f);
+	ImGui::SliderFloat("Box 1 Scale", &m_Scalef1, 50.0f, 500.0f);
+	ImGui::SliderFloat("Box 2 Scale", &m_Scalef2, 50.0f, 500.0f);
+	ImGui::SliderFloat("Box 3 Scale", &m_Scalef3, 50.0f, 500.0f);
+	ImGui::SliderFloat("Box 4 Scale", &m_Scalef4, 50.0f, 500.0f);
 	ImGui::End();
 
 	m_Position1.x = m_pos1[0] + 50.0f;
 	m_Position1.y = m_pos1[1] + 50.0f;
 	m_Position2.x = m_pos2[0] + 50.0f;
 	m_Position2.y = m_pos2[1] + 50.0f;
+	m_Position3.x = m_pos3[0] + 50.0f;
+	m_Position3.y = m_pos3[1] + 50.0f;
+	m_Position4.x = m_pos4[0] + 50.0f;
+	m_Position4.y = m_pos4[1] + 50.0f;
 		
 	m_Colour1 = { m_Colour1[0], m_Colour1[1], m_Colour1[2], m_Colour1[3] };
 	m_Colour2 = { m_Colour2[0], m_Colour2[1], m_Colour2[2], m_Colour2[3] };
+	m_Colour3 = { m_Colour3[0], m_Colour3[1], m_Colour3[2], m_Colour2[3] };
+	m_Colour4 = { m_Colour4[0], m_Colour4[1], m_Colour4[2], m_Colour3[3] };
+
+	m_Scale1 = { m_Scalef1, m_Scalef1 };
+	m_Scale2 = { m_Scalef2, m_Scalef2 };
+	m_Scale3 = { m_Scalef3, m_Scalef3 };
+	m_Scale4 = { m_Scalef4, m_Scalef4 };
 }
 
 SandboxLayer::~SandboxLayer() {
--- Sandbox/src/SandboxLayer.h
+++ Sandbox/src/SandboxLayer.h
@@ -17,7 +17,11 @@ public:
 private:
 	TE::OrthographicCamera* m_Camera = new TE::OrthographicCamera(0.0f, 1280.0f, 0.0f, 720.0f);
 
-	float m_pos1[2]{ 0.0f, 0.0f }, m_pos2[2]{ 0.0f, 0.0f };
+	float m_pos1[2]{ 0.0f, 0.0f }, m_pos2[2]{ 0.0f, 0.0f }, 
+		  m_pos3[2]{ 0.0f, 0.0f }, m_pos4[2]{ 0.0f, 0.0f };
+
+	float m_Scalef1 = 100.0f, m_Scalef2 = 100.0f, 
+		  m_Scalef3 = 100.0f, m_Scalef4 = 100.0f;
 
 	glm::vec2 m_Position1;
 	glm::vec2 m_Scale1;
@@ -26,4 +30,12 @@ private:
 	glm::vec2 m_Position2;
 	glm::vec2 m_Scale2;
 	glm::vec4 m_Colour2;
+
+	glm::vec2 m_Position3;
+	glm::vec2 m_Scale3;
+	glm::vec4 m_Colour3;
+
+	glm::vec2 m_Position4;
+	glm::vec2 m_Scale4;
+	glm::vec4 m_Colour4;
 };
